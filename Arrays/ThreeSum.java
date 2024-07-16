f24cd8ce056553a05843f3a6dc1da96330330726
AtulKumar2004 <akdeoghar16@gmail.com>
2024-07-16 12:58:53 +0530

    Modified

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/3sum/description/
+import java.util.*;
+
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> list1 = new ArrayList<>();
