ae56b52d60bcb1539e6b50317437b776f262fa92
AtulKumar2004 <akdeoghar16@gmail.com>
2024-07-20 10:50:40 +0530

    Single Element Problem

---
+++ Arrays/SingleElement.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/single-element-in-a-sorted-array/
+public class SingleElement {
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[nums.length-1] != nums[nums.length-2]) return nums[nums.length-1];
+
+        int start = 1;
+        int end = nums.length-2;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) return nums[mid];
+            if((mid % 2 == 1 && nums[mid] == nums[mid-1]) || (mid % 2 == 0) && nums[mid] == nums[mid+1]) start = mid + 1;
+            else end = mid - 1;
+        }  
+
+        return -1;
+    }
+}
