3a2a836c7eff3e446b1fc25ebd221c95aed4d168
AtulKumar2004 <akdeoghar16@gmail.com>
2024-08-14 08:25:26 +0530

    Prime Factors Problem

---
+++ Bit Manipulation/PrimeFactors.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class PrimeFactors {
+    public static int[] AllPrimeFactors(int N)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=2;i<=Math.sqrt(N);i++) {
+            if(N%i == 0) {
+                list.add(i);
+                while(N%i == 0) {
+                    N = N / i;
+                }
+            }
+        }
+        if(N != 1) list.add(N);
+        int ans[] = list.stream().mapToInt(i -> i).toArray();
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(AllPrimeFactors(100)));
+
+    }
+}
